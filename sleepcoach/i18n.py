5a29606939551c1ad42ebff89f85c3c566100699
Daniel Nylander <daniel@danielnylander.se>
2026-03-16 16:11:28 +0100

    i18n: proper locale/gettext setup with LANG support

--- sleepcoach/i18n.py
+++ sleepcoach/i18n.py
@@ -3,13 +3,28 @@ import locale
 import os
 
 DOMAIN = "sleepcoach"
-LOCALEDIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "locale")
+LOCALEDIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "locale")
+
+# System locale dirs
+SYSTEM_LOCALEDIRS = [
+    LOCALEDIR,
+    "/usr/share/locale",
+    "/usr/local/share/locale",
+]
 
 try:
     locale.setlocale(locale.LC_ALL, "")
 except locale.Error:
     pass
 
-gettext.bindtextdomain(DOMAIN, LOCALEDIR)
+# Find locale dir with our translations
+for d in SYSTEM_LOCALEDIRS:
+    if os.path.isdir(d):
+        gettext.bindtextdomain(DOMAIN, d)
+        break
+else:
+    gettext.bindtextdomain(DOMAIN, LOCALEDIR)
+
 gettext.textdomain(DOMAIN)
 _ = gettext.gettext
+
